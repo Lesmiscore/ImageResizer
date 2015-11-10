851fc9c813b8460a8ba7faf0f4a64426f568f101
nao20010128nao <nao20010128@gmail.com>
2015-11-10 17:21:27 +0900

    いろいろ

--- src/com/nao20010128nao/Image/Resizer/RectResize.java
+++ src/com/nao20010128nao/Image/Resizer/RectResize.java
@@ -7,6 +7,7 @@ import android.util.*;
 import java.io.*;
 import android.net.*;
 import java.net.*;
+import android.widget.*;
 
 public class RectResize extends Activity
 {
@@ -14,36 +15,45 @@ public class RectResize extends Activity
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
-		String data=getIntent().getDataString();
-		if(data==null)data=((Object)getIntent().getParcelableExtra(Intent.EXTRA_STREAM)).toString();
-		Bitmap bmp;InputStream is=null;OutputStream os=null;
-		try {
-			bmp=BitmapFactory.decodeStream(is=tryOpen(data));
-		} catch (Throwable e) {
-			finish();
-			return;
-		}finally{
-			try {
-				is.close();
-			} catch (Throwable e) {}
-		}
-		int size=bmp.getWidth()*bmp.getHeight();
-		int square=(int)Math.ceil(Math.sqrt(size));
-		Bitmap resized=Bitmap.createScaledBitmap(bmp,square,square,false);
-		File f=new File(Environment.getExternalStorageDirectory(),"ImageResizer");
-		if(!f.exists())f.mkdirs();
-		f=new File(f,System.currentTimeMillis()+".png");
-		try {
-			Log.d("dbg", "save:" + resized.compress(Bitmap.CompressFormat.PNG, 100, os = new FileOutputStream(f)));
-		} catch (Throwable e) {
-			finish();
-		}finally{
-			try {
-				os.flush();
-				os.close();
-			} catch (Throwable ez) {}
-		}
-		finish();
+		new Thread(){
+			public void run(){
+				String data=getIntent().getDataString();
+				if(data==null)data=((Object)getIntent().getParcelableExtra(Intent.EXTRA_STREAM)).toString();
+				Bitmap bmp;InputStream is=null;OutputStream os=null;
+				try {
+					bmp=BitmapFactory.decodeStream(is=tryOpen(data));
+				} catch (Throwable e) {
+					finish();
+					return;
+				}finally{
+					try {
+						is.close();
+					} catch (Throwable e) {}
+				}
+				int size=bmp.getWidth()*bmp.getHeight();
+				int square=(int)Math.ceil(Math.sqrt(size));
+				Bitmap resized=Bitmap.createScaledBitmap(bmp,square,square,false);
+				bmp.recycle();
+				File f=new File(Environment.getExternalStorageDirectory(),"ImageResizer");
+				if(!f.exists())f.mkdirs();
+				f=new File(f,System.currentTimeMillis()+".png");
+				try {
+					Log.d("dbg", "save:" + resized.compress(Bitmap.CompressFormat.PNG, 100, os = new FileOutputStream(f)));
+				} catch (Throwable e) {
+					finish();
+				}finally{
+					try {
+						os.flush();
+						os.close();
+					} catch (Throwable ez) {}
+					resized.recycle();
+				}
+				finish();
+			}
+		}.start();
+		TextView tv;
+		setContentView(tv=new TextView(this));
+		tv.setText("Please wait...");
 	}
 	public InputStream tryOpen(String uri) throws IOException {
 		Log.d("dbg", "tryOpen:" + uri);
@@ -54,5 +64,15 @@ public class RectResize extends Activity
 		} else {
 			return URI.create(uri).toURL().openConnection().getInputStream();
 		}
+	}
+
+	@Override
+	public void finish() {
+		// TODO: Implement this method
+		runOnUiThread(new Runnable(){
+			public void run(){
+				RectResize.super.finish();
+			}
+		});
 	}
 }
---
+++ src/com/nao20010128nao/Image/Resizer/RectResizeMulti.java
@@ -0,0 +1,81 @@
+package com.nao20010128nao.Image.Resizer;
+import android.app.*;
+import android.os.*;
+import android.content.*;
+import android.graphics.*;
+import android.util.*;
+import java.io.*;
+import android.net.*;
+import java.net.*;
+import android.widget.*;
+import java.util.*;
+
+public class RectResizeMulti extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		new Thread(){
+			public void run(){
+				ArrayList files=(ArrayList)getIntent().getSerializableExtra(Intent.EXTRA_STREAM);
+				ArrayList<String> stringFiles=new ArrayList();
+				for(Object o:files)stringFiles.add(o+"");
+				for (String data:stringFiles) {
+					Bitmap bmp;InputStream is=null;OutputStream os=null;
+					try {
+						bmp = BitmapFactory.decodeStream(is = tryOpen(data));
+					} catch (Throwable e) {
+						continue;
+					} finally {
+						try {
+							is.close();
+						} catch (Throwable e) {}
+					}
+					int size=bmp.getWidth() * bmp.getHeight();
+					int square=(int)Math.ceil(Math.sqrt(size));
+					Bitmap resized=Bitmap.createScaledBitmap(bmp, square, square, false);
+					bmp.recycle();
+					File f=new File(Environment.getExternalStorageDirectory(), "ImageResizer");
+					if (!f.exists())f.mkdirs();
+					f = new File(f, System.currentTimeMillis() + ".png");
+					try {
+						Log.d("dbg", "save:" + resized.compress(Bitmap.CompressFormat.PNG, 100, os = new FileOutputStream(f)));
+					} catch (Throwable e) {
+						finish();
+					} finally {
+						try {
+							os.flush();
+							os.close();
+						} catch (Throwable ez) {}
+						resized.recycle();
+					}
+				}
+				finish();
+			}
+		}.start();
+		TextView tv;
+		setContentView(tv=new TextView(this));
+		tv.setText("Please wait...");
+	}
+	public InputStream tryOpen(String uri) throws IOException {
+		Log.d("dbg", "tryOpen:" + uri);
+		if (uri.startsWith("content://")) {
+			return getContentResolver().openInputStream(Uri.parse(uri));
+		} else if (uri.startsWith("/")) {
+			return new FileInputStream(uri);
+		} else {
+			return URI.create(uri).toURL().openConnection().getInputStream();
+		}
+	}
+
+	@Override
+	public void finish() {
+		// TODO: Implement this method
+		runOnUiThread(new Runnable(){
+			public void run(){
+				RectResizeMulti.super.finish();
+			}
+		});
+	}
+}
