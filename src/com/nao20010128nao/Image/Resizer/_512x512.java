45888db4a9ca5ba45b05ec0ce5ae5e3b0abbd788
nao20010128nao <nao20010128@gmail.com>
2016-01-14 21:08:45 +0900

    追加

---
+++ src/com/nao20010128nao/Image/Resizer/_512x512.java
@@ -0,0 +1,77 @@
+package com.nao20010128nao.Image.Resizer;
+import android.app.*;
+import android.os.*;
+import android.graphics.*;
+import java.io.*;
+import android.content.*;
+import android.util.*;
+import android.widget.*;
+import android.net.*;
+import java.net.*;
+
+public class _512x512 extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		new Thread(){
+			public void run(){
+				OutputStream os=null;
+				String data=getIntent().getDataString();
+				if(data==null)data=((Object)getIntent().getParcelableExtra(Intent.EXTRA_STREAM)).toString();
+				Bitmap bmp;InputStream is=null;
+				try {
+					bmp=BitmapFactory.decodeStream(is=tryOpen(data));
+				} catch (Throwable e) {
+					finish();
+					return;
+				}finally{
+					try {
+						is.close();
+					} catch (Throwable e) {}
+				}
+				Bitmap resized=Bitmap.createScaledBitmap(bmp,512,512,false);
+				bmp.recycle();
+				File f=new File(Environment.getExternalStorageDirectory(),"ImageResizer");
+				if(!f.exists())f.mkdirs();
+				f=new File(f,System.currentTimeMillis()+".png");
+				try {
+					Log.d("dbg", "save:" + resized.compress(Bitmap.CompressFormat.PNG, 100, os = new FileOutputStream(f)));
+				} catch (Throwable e) {
+					finish();
+				}finally{
+					try {
+						os.flush();
+						os.close();
+					} catch (Throwable ez) {}
+					resized.recycle();
+				}
+				finish();
+			}
+		}.start();
+		TextView tv;
+		setContentView(tv=new TextView(this));
+		tv.setText("Please wait...");
+	}
+	public InputStream tryOpen(String uri) throws IOException {
+		Log.d("dbg", "tryOpen:" + uri);
+		if (uri.startsWith("content://")) {
+			return getContentResolver().openInputStream(Uri.parse(uri));
+		} else if (uri.startsWith("/")) {
+			return new FileInputStream(uri);
+		} else {
+			return URI.create(uri).toURL().openConnection().getInputStream();
+		}
+	}
+
+	@Override
+	public void finish() {
+		// TODO: Implement this method
+		runOnUiThread(new Runnable(){
+				public void run(){
+					_512x512.super.finish();
+				}
+			});
+	}
+}
